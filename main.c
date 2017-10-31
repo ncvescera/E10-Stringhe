954bb0087c286480bbc7ba919e9aa7103cb01ce2
Nicolò Vescera <nicolo.vescera@gmail.com>
2017-10-31 16:39:36 +0100

    Completato esercizio

--- main.c
+++ main.c
@@ -1,18 +1,37 @@
-/* 
- * File:   main.c
- * Author: ncvescera
- *
- * Created on 31 ottobre 2017, 16.25
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
+int strlength(const char *string);
+void inverse(char *string);
+
 int main(int argc, char** argv) {
+
+    char stringa[] = "ciao";
+
+    inverse(stringa);
+
+    printf("%s\n", stringa);
 
     return (EXIT_SUCCESS);
 }
 
+int strlength(const char *string) {
+    int count = 0;
+    for (int i = 0; string[i] != '\0'; i++)
+        count++;
+
+    //printf("%d\n",count );
+    return count;
+}
+
+void inverse(char *string) {
+    int slen = strlength(string) - 1;
+    char temp;
+
+    for (int i = 0; i < slen - 1; i++) {
+        temp = string[i];
+        string[i] = string[slen - i];
+        string[slen - i] = temp;
+
+    }
+}
