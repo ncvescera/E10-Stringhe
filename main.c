9ad47edf23a15dc13f65005fe60813a68e31edcb
Nicolò Vescera <nicolo.vescera@gmail.com>
2017-10-31 16:38:09 +0100

    Completato esercizio

--- main.c
+++ main.c
@@ -1,18 +1,30 @@
-/* 
- * File:   main.c
- * Author: ncvescera
- *
- * Created on 31 ottobre 2017, 16.25
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
+#define UPPER 'A'-'a'
+
+int toUpperChar(char c);
+void toUpperString(char *string);
+
 int main(int argc, char** argv) {
+
+    char stringa[] = "nicolo";
+
+    toUpperString(stringa);
+
+    puts(stringa);
 
     return (EXIT_SUCCESS);
 }
 
+int toUpperChar(char c) {
+    if (c >= 'a' && c <= 'z')
+        return (c + UPPER);
+    else
+        return c;
+}
+
+void toUpperString(char *string) {
+    for (int i = 0; string[i] != '\0'; i++)
+        string[i] = toUpperChar(string[i]);
+}
