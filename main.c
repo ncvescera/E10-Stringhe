73e0f00a3780bca6fd6ef0f1748f4f23f097c285
Nicolò Vescera <nicolo.vescera@gmail.com>
2017-10-31 16:35:45 +0100

    Completato esercizio

--- main.c
+++ main.c
@@ -1,18 +1,27 @@
-/* 
- * File:   main.c
- * Author: ncvescera
- *
- * Created on 31 ottobre 2017, 16.25
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
+int countCharInString(const char *string, char c);
+void countAllInString(const char *string);
+
 int main(int argc, char** argv) {
+
+    countAllInString("nicccccolo");
 
     return (EXIT_SUCCESS);
 }
 
+int countCharInString(const char *string, char c) {
+    int count = 0;
+
+    for (int i = 0; string[i] != '\0'; i++)
+        if (string[i] == c)
+            count++;
+
+    return count;
+}
+
+void countAllInString(const char *string) {
+    for (int i = 'a'; i < 'z'; i++)
+        printf("%c --> %d\n", i, countCharInString(string, i));
+}
